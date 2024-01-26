2b2f9936672fdc960081c7c2f9345c1b1dbf55f3
irvelds <irvelds@mail.ru>
2024-01-27 00:01:20 +0300

    feat: add hash and streams commands

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from "url";
+import path, { dirname } from 'path';
+import { readFile } from 'fs/promises';
+import crypto from 'crypto';
+
+const currentPath = fileURLToPath(import.meta.url);
+const targetPath = path.join(dirname(currentPath), 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const contentFile = await readFile(targetPath);
+        const hash = crypto.createHash('sha256').update(contentFile);
+        const digest = hash.digest('hex');
+        console.log(digest);
+    }
+    catch (err) {
+        console.error(err);
+    }
 };
 
 await calculateHash();
